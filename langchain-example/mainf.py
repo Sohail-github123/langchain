d9e5e5a3c0e7a0b2e9353466ba5bf5fe65cc451e
Sohail-github123 <150891088+Sohail-github123@users.noreply.github.com>
2024-04-10 00:09:15 +0530

    Add files via upload

---
+++ langchain-example/mainf.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import langchain_helper as lch
+
+st.title("Pets Name Generator")
+
+user_animal_type = st.sidebar.selectbox("What is your pet?", ("Dog", "Cat", "Hamster", "Cow"))
+
+if user_animal_type == 'Dog':
+    pet_color =st.sidebar.text_area(label='what color is your Dog?',
+    max_chars=15)
+
+if user_animal_type == 'cat':
+    pet_color =st.sidebar.text_area(label='what color is your cat?',
+    max_chars=15)
+
+if user_animal_type == 'Hamster':
+    pet_color =st.sidebar.text_area(label='what color is your Hamster?',
+    max_chars=15)
+
+if user_animal_type == 'cow':
+    pet_color =st.sidebar.text_area(label='what color is your cow?',
+    max_chars=15)
